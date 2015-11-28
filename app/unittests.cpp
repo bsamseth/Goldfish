46c862d6ecdccedc3df4500a62eeb6836c413e7f
Bendik Samseth <b.samseth@gmail.com>
2015-11-28 17:28:10 +0100

    Changed to using cmake
    This is much nicer than regular make.
    
    New structure:
    
    - Header files in dir "include"
    - Files with mains placed in dir "app"
    - Other sourcefiles in dir "src"
    - build is done in seperate folder, "build"

---
+++ app/unittests.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#include "types.h"
+#include "move.h"
+#include "position.h"
+#include "bitboards.h"
+#include "movegen.h"
+
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using Bitboards::prettyString;
+
+
+int main(int argc, char *argv[])
+{
+  cout << "yey" << endl;
+  return 0;
+}
