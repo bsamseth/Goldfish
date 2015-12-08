a7d542f8528c2acc76721a6c918e293872336579
Bendik Samseth <b.samseth@gmail.com>
2015-12-08 20:30:31 +0100

    Absolute value if StateInfo is not known, test diff.

--- tests/stateinfo.cpp
+++ tests/stateinfo.cpp
@@ -11,9 +11,8 @@ using namespace std;
 TEST (StateInfo, init) {
   StateInfo s = {}, s2 = {};
   StateInfo s3 = {};
-  EXPECT_EQ(0, s.id);
-  EXPECT_EQ(1, s2.id);
-  EXPECT_EQ(2, s3.id);
+  EXPECT_EQ(1, s2.id-s.id);
+  EXPECT_EQ(2, s3.id-s.id);
   EXPECT_EQ(NO_PIECE, s2.lastMove_destinationPiece);
   EXPECT_EQ(NO_SQUARE, s.lastMove_enpassantTarget);
   EXPECT_EQ(nullptr, s3.previous);
