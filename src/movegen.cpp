be539e03763ad00b89176be23202c791c8ba6f68
Bendik Samseth <b.samseth@gmail.com>
2017-07-12 22:06:52 +1000

    Rewrite move generation.
    Inspired from Sunfish. As it is, checks for own king in check
    after move is not done. Castling is currently possible through a enemy
    ray piece, fixed by the use of king enpassant.
    
    The thinking is that moves that leave the king open for capture *could*
    be generated, but they will fail quickly because the king is lost on
    next move. So a searcher should never end up giving a move that is
    illegal. At least that is the current justification.

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <map>
+#include <vector>
 
 #include "types.h"
 #include "bitboards.h"
@@ -20,7 +22,7 @@ MoveGenerator::MoveGenerator() {}
  * Initialize the generator with a position.
  */
 MoveGenerator::MoveGenerator(Position& position) {
-  pos = &position;
+    pos = &position;
 }
 
 /*
@@ -29,27 +31,98 @@ MoveGenerator::MoveGenerator(Position& position) {
  * current position. All moves will be encoded with a
  * proper flag.
  */
-void MoveGenerator::generateMoves() {
-  Square cSquare, target;
-    
-  for (int rank = RANK_1; rank <= RANK_8; rank++) {
-    for (int file = FILE_A; file <= FILE_H; file++) {
+/* void MoveGenerator::generateMoves() { */
+/*   Square cSquare, target; */
 
-      cSquare = Square(rank*8 + file);
-      if (!pos->occupied(cSquare, pos->sideToMove)) {
-	continue;
-      }
-      
-      for (int rank2 = RANK_1; rank2 <= RANK_8; rank2++) {
-	for (int file2 = FILE_A; file2 <= FILE_H; file2++) {
-	  target = Square(rank2*8 + file2);
-	  if (cSquare != target && pos->legal(Move(cSquare, target)))
-	    // cout << "found a move : " << Move(cSquare, target).str() << endl;
-	    encodeAndAddMove(cSquare, target);
-	}
-      }
+/*   for (int rank = RANK_1; rank <= RANK_8; rank++) { */
+/*     for (int file = FILE_A; file <= FILE_H; file++) { */
+
+/*       cSquare = Square(rank*8 + file); */
+/*       if (!pos->occupied(cSquare, pos->sideToMove)) { */
+/* 	continue; */
+/*       } */
+
+/*       for (int rank2 = RANK_1; rank2 <= RANK_8; rank2++) { */
+/* 	for (int file2 = FILE_A; file2 <= FILE_H; file2++) { */
+/* 	  target = Square(rank2*8 + file2); */
+/* 	  if (cSquare != target && pos->legal(Move(cSquare, target))) */
+/* 	    // cout << "found a move : " << Move(cSquare, target).str() << endl; */
+/* 	    encodeAndAddMove(cSquare, target); */
+/* 	} */
+/*       } */
+/*     } */
+/*   } */
+/* } */
+
+
+void MoveGenerator::generateMoves() {
+    Square cSquare, target;
+    for (int rank = RANK_1; rank <= RANK_8; rank++) {
+        for (int file = FILE_A; file <= FILE_H; file++) {
+            cSquare = Square(rank*8 + file);
+            if (!pos->occupied(cSquare, pos->sideToMove)) {
+                continue;
+            }
+            Piece p = pos->board[cSquare];
+            PieceType pt = makePieceType(p);
+            for (auto d : directions.find(p)->second) {
+                target = cSquare + d;
+                while (true) {
+                    if ( target < SQ_A1 or target > SQ_H8 ) break;
+                    if (pos->occupied(target, pos->sideToMove)) break;
+                    Piece q = pos->board[target];
+
+                    /* PAWN RULES */
+                    // Pawn push with blocking piece.
+                    if (pt == PAWN and (d == D_NORTH or d == D_NORTH + D_NORTH
+                                or d == D_SOUTH or d == D_SOUTH + D_SOUTH)
+                            and q != NO_PIECE)
+                        break;
+                    // Double pawn push not on starting rank or blocking piece.
+                    if (pt == PAWN and d == D_NORTH + D_NORTH
+                            and (((1ULL << cSquare) & RANK_2_BB) == 0 or pos->occupied(cSquare + D_NORTH)))
+                        break;
+                    if (pt == PAWN and d == D_SOUTH + D_SOUTH
+                            and (((1ULL << cSquare) & RANK_7_BB) == 0 or pos->occupied(cSquare + D_SOUTH)))
+                        break;
+                    // Pawn capture with no piece to capture (and not ep).
+                    if (pt == PAWN and (d == D_NORTH_EAST or d == D_NORTH_WEST or
+                                d == D_SOUTH_EAST or d == D_SOUTH_WEST)
+                            and q == NO_PIECE and target != pos->enpassantTarget)
+                        break;
+
+                    /* PIECE RULES */
+                    int d_rank = abs(rank_diff(cSquare, target));
+                    int d_file = abs(file_diff(cSquare, target));
+
+                    if (pt == KNIGHT and (d_rank + d_file != 3)) break;
+                    if (pt == BISHOP and d_file != d_rank) break;
+                    if (pt == ROOK and not ((d_file == 0) != (d_rank == 0))) break;
+                    if ((pt == QUEEN or pt == KING) and not ((d_file == d_rank) or ((d_file == 0) != (d_rank == 0)))) break;
+
+                    encodeAndAddMove(cSquare, target);
+
+                    // Stop crawlers from sliding, and sliding after captures.
+                    if (pt == PAWN or pt == KNIGHT or pt == KING or q != NO_PIECE)
+                        break;
+
+                    // Castle by sliding the rock next to the king.
+                    if (((cSquare == SQ_A1 and (pos->castlingRights & WHITE_OOO)) or
+                                (cSquare == SQ_A8 and (pos->castlingRights & BLACK_OOO)))
+                            and makePieceType(pos->board[target + D_EAST]) == KING
+                            and makeColor(pos->board[target + D_EAST]) == pos->sideToMove)
+                        encodeAndAddMove(target + D_EAST, target + D_WEST);
+                    if (((cSquare == SQ_H1 and (pos->castlingRights & WHITE_OO)) or
+                                (cSquare == SQ_H8 and (pos->castlingRights & BLACK_OO)))
+                            and makePieceType(pos->board[target + D_WEST]) == KING
+                            and makeColor(pos->board[target + D_WEST]) == pos->sideToMove)
+                        encodeAndAddMove(target + D_WEST, target + D_EAST);
+
+                    target += d;
+                }
+            }
+        }
     }
-  }
 }
 
 /*
@@ -57,62 +130,65 @@ void MoveGenerator::generateMoves() {
  * It assumes that pos->legal(s1, s2) has returned true.
  */
 void MoveGenerator::encodeAndAddMove(Square s1, Square s2) {
-  Piece p1 = pos->board[s1], p2 = pos->board[s2];
-  Square up = makeColor(p1) == WHITE ? D_NORTH : D_SOUTH;
-  Square from = s1;
-  Square to   = s2;
+    Piece p1 = pos->board[s1], p2 = pos->board[s2];
+    Square up = makeColor(p1) == WHITE ? D_NORTH : D_SOUTH;
+    Square from = s1;
+    Square to   = s2;
 
-  // castling
-  if (makePieceType(p1) == KING) {
-    if (s1 == SQ_E1 || s1 == SQ_E8) {
-      if (s2 == SQ_G1 || s2 == SQ_G8)
-	generatedMoves.push_back(Move(from, to, KING_CASTLE_MOVE));
-      else if (s2 == SQ_C1 || s2 == SQ_C8)
-	generatedMoves.push_back(Move(from, to, QUEEN_CASTLE_MOVE));
+    // castling
+    if (makePieceType(p1) == KING) {
+        if (s1 == SQ_E1 || s1 == SQ_E8) {
+            if (s2 == SQ_G1 || s2 == SQ_G8) {
+                generatedMoves.push_back(Move(from, to, KING_CASTLE_MOVE));
+                return;
+            } else if (s2 == SQ_C1 || s2 == SQ_C8) {
+                generatedMoves.push_back(Move(from, to, QUEEN_CASTLE_MOVE));
+                return;
+            }
+        }
     }
-  }
-  
-  // en passant?
-  if (pos->enpassantTarget != NO_SQUARE && makePieceType(p1) == PAWN && s2 == pos->enpassantTarget) { 
-    generatedMoves.push_back(Move(from, to, ENPASSANT_CAPTURE_MOVE));
-    return;
-  }
-	
-  // promotion?
-  if (makePieceType(p1) == PAWN && (s2 & RANK_8_BB || s2 & RANK_8_BB) )  {
-    // generate all the possible promotions
-    if (p2 != NO_PIECE) { // promo capture?
-      generatedMoves.push_back(Move(from, to, KNIGHT_PROMO_CAPTURE_MOVE));
-      generatedMoves.push_back(Move(from, to, BISHOP_PROMO_CAPTURE_MOVE));
-      generatedMoves.push_back(Move(from, to, ROOK_PROMO_CAPTURE_MOVE));
-      generatedMoves.push_back(Move(from, to, QUEEN_PROMO_CAPTURE_MOVE));
-    } else {
-      generatedMoves.push_back(Move(from, to, KNIGHT_PROMO_MOVE));
-      generatedMoves.push_back(Move(from, to, BISHOP_PROMO_MOVE));
-      generatedMoves.push_back(Move(from, to, ROOK_PROMO_MOVE));
-      generatedMoves.push_back(Move(from, to, QUEEN_PROMO_MOVE));
+
+    // en passant?
+    if (pos->enpassantTarget != NO_SQUARE && makePieceType(p1) == PAWN && s2 == pos->enpassantTarget) {
+        generatedMoves.push_back(Move(from, to, ENPASSANT_CAPTURE_MOVE));
+        return;
     }
-    return;
-  }
 
-  // capture?
-  if (p2 != NO_PIECE) {
-    generatedMoves.push_back(Move(from, to, CAPTURE_MOVE));
-    return;
-  }
+    // promotion?
+    if (makePieceType(p1) == PAWN && (s2 & RANK_8_BB || s2 & RANK_8_BB) )  {
+        // generate all the possible promotions
+        if (p2 != NO_PIECE) { // promo capture?
+            generatedMoves.push_back(Move(from, to, KNIGHT_PROMO_CAPTURE_MOVE));
+            generatedMoves.push_back(Move(from, to, BISHOP_PROMO_CAPTURE_MOVE));
+            generatedMoves.push_back(Move(from, to, ROOK_PROMO_CAPTURE_MOVE));
+            generatedMoves.push_back(Move(from, to, QUEEN_PROMO_CAPTURE_MOVE));
+        } else {
+            generatedMoves.push_back(Move(from, to, KNIGHT_PROMO_MOVE));
+            generatedMoves.push_back(Move(from, to, BISHOP_PROMO_MOVE));
+            generatedMoves.push_back(Move(from, to, ROOK_PROMO_MOVE));
+            generatedMoves.push_back(Move(from, to, QUEEN_PROMO_MOVE));
+        }
+        return;
+    }
 
-    
-  // double pawn push?
-  if (makePieceType(p1) == PAWN && (int(to) - int(from) == 2*int(up))) {
-    generatedMoves.push_back(Move(from, to, DOUBLE_PAWN_PUSH_MOVE));
-    return;
-  }
+    // capture?
+    if (p2 != NO_PIECE) {
+        generatedMoves.push_back(Move(from, to, CAPTURE_MOVE));
+        return;
+    }
 
-  generatedMoves.push_back(Move(from, to, QUIET_MOVE));
+
+    // double pawn push?
+    if (makePieceType(p1) == PAWN && (int(to) - int(from) == 2*int(up))) {
+        generatedMoves.push_back(Move(from, to, DOUBLE_PAWN_PUSH_MOVE));
+        return;
+    }
+
+    generatedMoves.push_back(Move(from, to, QUIET_MOVE));
 }
 
 Move MoveGenerator::getRandomMove() {
-  int randIndex = rand() % generatedMoves.size();
-  return generatedMoves[randIndex];
+    int randIndex = rand() % generatedMoves.size();
+    return generatedMoves[randIndex];
 }
 
