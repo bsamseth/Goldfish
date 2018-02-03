01bc312b06bb5f09775e7fff0368bbcad87959b2
Bendik Samseth <b.samseth@gmail.com>
2018-02-03 16:11:39 +0100

    Implement the MTD(f) search algorithm.
    Work in progress. Includes transposition table,
    killer move and quiescence search. Need to test for
    stability. Would also like to add null move pruning,
    and other pruning strategies eventually.

--- include/search.h
+++ include/search.h
@@ -5,22 +5,60 @@
 #include <array>
 #include <vector>
 
+#include "types.h"
+#include "move.h"
 #include "position.h"
+#include "LRUCache.h"
 
 const int MAX_SEARCH_DEPTH = 50;
+const uint64_t TABLE_SIZE = 1e8;
+const uint64_t CACHE_ELASTICITY = 0;
+
+struct Entry{
+    int lower, upper;
+};
+struct ScoreLookupKey {
+    Key hash;
+    int depth;
+    bool root;
+
+    bool operator==(const ScoreLookupKey &other) const {
+        return hash == other.hash and depth == other.depth and root == other.root;
+    }
+};
+namespace std {
+    template <>
+    struct hash<ScoreLookupKey> {
+        std::size_t operator()(const ScoreLookupKey &k) const {
+            // Combine hashes using XOR and bit shifting:
+            return ((k.hash ^ (std::hash<int>()(k.depth) << 1)) >> 1) ^ (hash<bool>()(k.root) << 1);
+        }
+    };
+}
 
 class Searcher {
     private:
-    std::array<Move, MAX_SEARCH_DEPTH> pv;
-    int alpha_beta_negamax(Position & pos, int depth, int alpha, int beta, int side, std::array<Move, MAX_SEARCH_DEPTH> & PV);
-    bool verbose;
-    int nodes;
+        bool verbose, interrupted = false;
+        int nodes;
+        LRU::Cache<Key, Move> tp_move;
+        LRU::Cache<ScoreLookupKey, Entry> tp_score;
+        std::array<Move, MAX_SEARCH_DEPTH> pv;
+
+        int alpha_beta_negamax(Position & pos, int depth, int alpha, int beta, int side,
+                std::array<Move, MAX_SEARCH_DEPTH> & PV);
+        int bound(Position& pos, int gamma, int depth, bool root = true);
+        bool check_move(Position &pos, const Move &m, int &best, int depth, int gamma);
 
     public:
-    Searcher();
-    Searcher(bool verbose);
-    std::string pvString() const;
-    int search_depth(Position & pos, int max_depth);
+        Searcher(bool verbose = true);
+        void interrupt();
+        std::string pvString() const;
+        int search_depth(Position & pos, int max_depth);
+        int search_depth_MTD(Position & pos, int max_depth);
 };
+
+inline void Searcher::interrupt() {
+    interrupted = true;
+}
 
 #endif /* SEARCH_H */
--- src/search.cpp
+++ src/search.cpp
@@ -16,9 +16,8 @@
 using std::cout;
 using std::endl;
 
-Searcher::Searcher() : verbose(true), nodes(0) {}
-
-Searcher::Searcher(bool v) : verbose(v), nodes(0) {}
+Searcher::Searcher(bool v) : verbose(v), nodes(0), tp_move(TABLE_SIZE, CACHE_ELASTICITY),
+                       tp_score(TABLE_SIZE, CACHE_ELASTICITY) {}
 
 /* Convert evaluation to string, handling output of mate. */
 std::string valueToString(int value, Color side) {
@@ -89,6 +88,7 @@ int Searcher::alpha_beta_negamax(Position & pos, int depth, int alpha, int beta,
     return best_value;
 }
 
+
 std::string Searcher::pvString() const {
     std::stringstream ss;
     for (int depth = pv.size() - 1; depth >= 0; depth--) {
@@ -100,3 +100,110 @@ std::string Searcher::pvString() const {
     return ss.str();
 }
 
+bool Searcher::check_move(Position &pos, const Move &m, int &best, int depth, int gamma) {
+    if (depth == 0) {
+        best = std::max(best, pos.maximizingScore());
+        if (best >= gamma)
+            return true;
+    }
+    if (depth > 0 or pos.value(m) >= QS_LIMIT) {
+        pos.doMove(m);
+        best = std::max(best, -bound(pos, 1 - gamma, depth - 1, false));
+        pos.undoMove();
+
+        if (best >= gamma) {
+            tp_move.insert(pos.hash, m);
+            return true;
+        }
+    }
+    return false;
+}
+
+int Searcher::bound(Position& pos, int gamma, int depth, bool root) {
+    nodes++;
+
+    depth = std::max(depth, 0);
+
+    // Since king-capture is possible, always check if we have a king.
+    if (pos.maximizingScore() <= -MATE_LOWER)
+        return -MATE_UPPER;
+
+
+    // Table lookup.
+    Entry entry = {-MATE_UPPER, MATE_UPPER};
+    if (tp_score.tryGet({pos.hash, depth, root}, entry)) {
+        Move m;
+        if (entry.lower >= gamma and (not root or tp_move.tryGet(pos.hash, m)))
+            return entry.lower;
+        if (entry.upper < gamma)
+            return entry.upper;
+    }
+
+    // Run through moves, shortcutting when possible.
+    int best = -MATE_UPPER;
+    MoveGenerator mg(pos);
+    mg.generateMoves();
+    mg.sortMoves();
+    Move killer;
+    if (tp_move.tryGet(pos.hash, killer))
+        mg.addKiller(killer);
+    for (const Move &m : mg.getGeneratedMoves()) {
+        if (interrupted or check_move(pos, m, best, depth, gamma)) {
+            break;
+        }
+    }
+
+    // Table part 2.
+    if (not interrupted) {
+        if (best >= gamma)
+            tp_score.insert({pos.hash, depth, root}, {best, entry.upper});
+        if (best < gamma)
+            tp_score.insert({pos.hash, depth, root}, {entry.lower, best});
+    }
+
+    return best;
+}
+
+/* Iterative deepening MTD-bi search. */
+int Searcher::search_depth_MTD(Position & pos, int max_depth) {
+    int depth;
+    Entry value;
+    Move m;
+    for (depth = 1; depth <= max_depth and not interrupted; depth++) {
+        nodes = 0; // Reset node counter.
+
+
+        int score, gamma;
+        value = {-MATE_LOWER, MATE_UPPER};
+        while (value.lower < value.upper - EVAL_ROUGHNESS and not interrupted) {
+            gamma = (value.lower + value.upper + 1) / 2;
+            score = bound(pos, gamma, depth);
+            if (score >= gamma)
+                value.lower = score;
+            if (score < gamma)
+                value.upper = score;
+        }
+
+        // Ensure move to play is in tp_move by another call
+        // that must faill high and therefore produce a move.
+        score = bound(pos, value.lower, depth);
+        m = tp_move.get(pos.hash);
+        if (verbose) {
+            printf("info nodes %d depth %d score cp %d pv %s\n",
+                   nodes, depth,
+                   /* valueToString(value.lower*sign, pos.sideToMove).c_str(), */
+                   value.lower,
+                   m.str().c_str());
+            std::cout << std::flush;
+        }
+        // If a mate is found there is no need to continue.
+        if (std::abs(value.lower) >= MATE_LOWER) {
+            break;
+        }
+    }
+    if (verbose) {
+        printf("bestmove %s\n", m.str().c_str());
+    }
+    interrupted = false;
+    return value.lower;
+}
