1cf4c1c4bd5f4754c93709f9b449eca0e2253fee
Bendik Samseth <b.samseth@gmail.com>
2018-11-11 17:23:07 +0100

    Implement TranspositionTable (not used in search yet).

--- include/tt.hpp
+++ include/tt.hpp
@@ -7,22 +7,23 @@
 #include "bound.hpp"
 #include "move.hpp"
 
-namespace goldfish {
+namespace goldfish::tt {
 
-struct __attribute__ ((packed)) TTEntry  {
-    uint32_t key;
-    Value value;
-    Bound bound;
-    Depth depth;
-    Move move;
+struct  TTEntry  {
+    uint32_t key32;
+    uint32_t move32;
+    int16_t value16;
+    uint8_t bound8;
+    uint8_t depth8;
 
-    void save(uint32_t key, Value value, Bound bound, Depth depth, Move move) {
-        this->key = key;
-        this->value = value;
-        this->bound = bound;
-        this->depth = depth;
-        this->move = move;
-    }
+    uint32_t key() const { return key32; }
+    Move move() const { return Move(move32); }
+    Value value() const { return Value(value16); }
+    Bound bound() const { return Bound(bound8); }
+    Depth depth() const { return Depth(depth8); }
+
+
+    void save(uint64_t key, Value value, Bound bound, Depth depth, Move move);
 };
 
 // If the compiler does not support packed attribute (GCC feature), then the size might not be
@@ -37,22 +38,26 @@ class TranspositionTable {
 
     public:
 
-        const TTEntry* probe(const uint64_t key) const {
-            const TTEntry* tte = &table[static_cast<uint32_t>(key) % size];
-            if (tte->key == key >> 32) {
-                return tte;
-            }
-            return nullptr;
-        }
-
-        void store(const uint64_t key, Value value, Bound bound, Depth depth, Move move) {
-            TTEntry *tte = &table[static_cast<uint32_t>(key) % size];
-
-            // Store new entry if depth is greater than existing entry.
-            if (tte->depth < depth) {
-                tte->save(key >> 32, value, bound, depth, move);
-            }
-        }
+        const TTEntry* probe(const uint64_t key) const;
+        void store(const uint64_t key, Value value, Bound bound, Depth depth, Move move);
 };
+
+
+template<int Mb_Size>
+const TTEntry* TranspositionTable<Mb_Size>::probe(const uint64_t key) const {
+    const TTEntry* tte = &table[static_cast<uint32_t>(key) % size];
+    if (tte->key() == key >> 32) {
+        return tte;
+    }
+    return nullptr;
+}
+
+template<int Mb_Size>
+void TranspositionTable<Mb_Size>::store(const uint64_t key, Value value, Bound bound, Depth depth, Move move) {
+    TTEntry *tte = &table[static_cast<uint32_t>(key) % size];
+
+    // Update preference implemented in save, not handled here.
+    tte->save(key, value, bound, depth, move);
+}
 
 }
