579c2c6928921ee401007a4ebf8de14c128f1de0
Bendik Samseth <b.samseth@gmail.com>
2019-02-15 15:46:13 +0100

    Move Bitboards from class to namespace
    Left over from original fluxroot/pulse impl., where
    classes with only static functions was used instead of namespaces.
    
    Also made everything constexpr, which might help optimizations a smidge.

--- include/bitboard.hpp
+++ include/bitboard.hpp
@@ -5,29 +5,61 @@
 
 namespace goldfish {
 
-class Bitboard {
-public:
-    static uint64_t add(int square, uint64_t bitboard);
+using U64 = uint64_t;
 
-    static uint64_t remove(int square, uint64_t bitboard);
+namespace Bitboard {
 
-    static int next(uint64_t bitboard);
+constexpr U64 DEBRUIJN64 = 0x03F79D71B4CB0A89ULL;
 
-    static uint64_t remainder(uint64_t bitboard);
-
-    static int size(uint64_t bitboard);
-
-    static int number_of_trailing_zeros(uint64_t b);
-
-    static int bit_count(uint64_t b);
-
-private:
-    static const uint64_t DEBRUIJN64 = 0x03F79D71B4CB0A89ULL;
-    static const std::array<int, 64> lsb_table;
-
-    static int to_x88_square(int square);
-
-    static int to_bit_square(int square);
+constexpr std::array<int, 64> lsb_table = {
+        0, 47, 1, 56, 48, 27, 2, 60,
+        57, 49, 41, 37, 28, 16, 3, 61,
+        54, 58, 35, 52, 50, 42, 21, 44,
+        38, 32, 29, 23, 17, 11, 4, 62,
+        46, 55, 26, 59, 40, 36, 15, 53,
+        34, 51, 20, 43, 31, 22, 10, 45,
+        25, 39, 14, 33, 19, 30, 9, 24,
+        13, 18, 8, 12, 7, 6, 5, 63
 };
 
+constexpr int bit_count(U64 b) {
+    b = b - ((b >> 1) & 0x5555555555555555ULL);
+    b = (b & 0x3333333333333333ULL) + ((b >> 2) & 0x3333333333333333ULL);
+    b = (b + (b >> 4)) & 0x0F0F0F0F0F0F0F0FULL;
+    return (b * 0x0101010101010101ULL) >> 56;
+}
+
+constexpr int number_of_trailing_zeros(U64 b) {
+    return lsb_table[((b ^ (b - 1)) * DEBRUIJN64) >> 58];
+}
+
+constexpr int to_bit_square(int square) {
+    return ((square & ~7) >> 1) | (square & 7);
+}
+
+constexpr int to_x88_square(int square) {
+    return ((square & ~7) << 1) | (square & 7);
+}
+
+constexpr U64 add(int square, U64 bitboard) {
+    return bitboard | 1ULL << to_bit_square(square);
+}
+
+constexpr U64 remove(int square, U64 bitboard) {
+    return bitboard & ~(1ULL << to_bit_square(square));
+}
+
+constexpr int next(U64 bitboard) {
+    return to_x88_square(number_of_trailing_zeros(bitboard));
+}
+
+constexpr U64 remainder(U64 bitboard) {
+    return bitboard & (bitboard - 1);
+}
+
+constexpr int size(U64 bitboard) {
+    return bit_count(bitboard);
+}
+
+}
 }
--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -1,56 +1,7 @@
 #include "bitboard.hpp"
 #include "square.hpp"
 
-namespace goldfish {
+namespace goldfish::Bitboard {
 
-const std::array<int, 64> Bitboard::lsb_table = {
-        0, 47, 1, 56, 48, 27, 2, 60,
-        57, 49, 41, 37, 28, 16, 3, 61,
-        54, 58, 35, 52, 50, 42, 21, 44,
-        38, 32, 29, 23, 17, 11, 4, 62,
-        46, 55, 26, 59, 40, 36, 15, 53,
-        34, 51, 20, 43, 31, 22, 10, 45,
-        25, 39, 14, 33, 19, 30, 9, 24,
-        13, 18, 8, 12, 7, 6, 5, 63
-};
-
-uint64_t Bitboard::add(int square, uint64_t bitboard) {
-    return bitboard | 1ULL << to_bit_square(square);
-}
-
-uint64_t Bitboard::remove(int square, uint64_t bitboard) {
-    return bitboard & ~(1ULL << to_bit_square(square));
-}
-
-int Bitboard::next(uint64_t bitboard) {
-    return to_x88_square(number_of_trailing_zeros(bitboard));
-}
-
-uint64_t Bitboard::remainder(uint64_t bitboard) {
-    return bitboard & (bitboard - 1);
-}
-
-int Bitboard::size(uint64_t bitboard) {
-    return bit_count(bitboard);
-}
-
-int Bitboard::to_x88_square(int square) {
-    return ((square & ~7) << 1) | (square & 7);
-}
-
-int Bitboard::to_bit_square(int square) {
-    return ((square & ~7) >> 1) | (square & 7);
-}
-
-int Bitboard::number_of_trailing_zeros(uint64_t b) {
-    return lsb_table[((b ^ (b - 1)) * DEBRUIJN64) >> 58];
-}
-
-int Bitboard::bit_count(uint64_t b) {
-    b = b - ((b >> 1) & 0x5555555555555555ULL);
-    b = (b & 0x3333333333333333ULL) + ((b >> 2) & 0x3333333333333333ULL);
-    b = (b + (b >> 4)) & 0x0F0F0F0F0F0F0F0FULL;
-    return (b * 0x0101010101010101ULL) >> 56;
-}
 
 }
