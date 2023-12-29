31e48e6b51db0295c1a686b944d824b802440ae9
Bendik Samseth <b.samseth@gmail.com>
2023-12-29 23:36:26 +0100

    fix clippy warnings

--- goldfish/src/uci/comm.rs
+++ goldfish/src/uci/comm.rs
@@ -1,16 +1,30 @@
+use crate::engine::Engine;
 use crate::uci::types::UciCommand;
 use crate::uci::Uci;
+
 use anyhow::Result;
 use chess::Game;
 use std::io::BufRead;
 
 impl Default for Uci {
     fn default() -> Self {
-        Self { game: Game::new() }
+        Self {
+            game: Game::new(),
+            engine: Engine {},
+        }
     }
 }
 
 impl Uci {
+    /// Start the UCI communication loop.
+    ///
+    /// This function will block until the `quit` command is received, or an error occurs.
+    ///
+    /// # Errors
+    /// This function will return an error if it fails to read from stdin.
+    ///
+    /// It will _not_ return an error if it encounters an invalid UCI command. In this case the
+    /// error message will be logged to stderr, and otherwise ignored.
     pub fn start(&mut self) -> Result<()> {
         let stdin = std::io::stdin();
         loop {
@@ -19,6 +33,7 @@ impl Uci {
                 .lines()
                 .next()
                 .ok_or_else(|| anyhow::anyhow!("Failed to read line from stdin"))??;
+
             let command = UciCommand::from(line.as_str());
             match command {
                 UciCommand::Unknown(e) => {
--- goldfish/src/uci/mod.rs
+++ goldfish/src/uci/mod.rs
@@ -1,10 +1,12 @@
 mod comm;
 mod types;
 
+use crate::engine::Engine;
 use chess::Game;
 
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct Uci {
     game: Game,
+    engine: Engine,
 }
