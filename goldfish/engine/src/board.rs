a4a3336bfe5fa59e5d699c66037bb6db21fe3977
Bendik Samseth <b.samseth@gmail.com>
2024-02-06 18:22:51 +0100

    Add draw detection to search

--- goldfish/engine/src/board.rs
+++ goldfish/engine/src/board.rs
@@ -1,6 +1,7 @@
-use chess::{Board, ChessMove};
+use chess::{Board, ChessMove, Piece};
 
 /// Extension trait for `Board` to add some utility methods.
+#[allow(clippy::module_name_repetitions)]
 pub trait BoardExt {
     /// Return true if the move would cause a reset of the 50-move rule counter.
     ///
@@ -10,39 +11,23 @@ pub trait BoardExt {
     /// Panics if there is no piece on the `from` square.
     fn halfmove_reset(&self, mv: ChessMove) -> bool;
 
-    /// Make a chess move onto an already allocated `Board`, and update the halfmove clock.
+    /// Return true if the board has insufficient material for either side to checkmate.
     ///
-    /// If the move is a capture or a pawn move, the halfmove clock will be reset to 0.
-    /// Otherwise, it will be incremented by 1.
-    /// The `halfmove_clock` parameter is updated in place.
-    ///
-    /// # Panics
-    /// Panics if there is no piece on the `from` square.
-    fn make_move_with_halfmove(
-        &self,
-        mv: ChessMove,
-        result: &mut Board,
-        halfmove_clock: &mut usize,
-    );
+    /// This does not include all technically insufficient material situations, but if this
+    /// returns true, it is guaranteed that the game is a draw regardless of the moves played.
+    fn has_insufficient_material(&self) -> bool;
 }
 
 impl BoardExt for Board {
     fn halfmove_reset(&self, mv: ChessMove) -> bool {
-        self.piece_on(mv.get_source()).unwrap() == chess::Piece::Pawn
+        self.piece_on(mv.get_source()).unwrap() == Piece::Pawn
             || self.piece_on(mv.get_dest()).is_some()
     }
 
-    fn make_move_with_halfmove(
-        &self,
-        mv: ChessMove,
-        result: &mut Board,
-        halfmove_clock: &mut usize,
-    ) {
-        if self.halfmove_reset(mv) {
-            *halfmove_clock = 0;
-        } else {
-            *halfmove_clock += 1;
-        }
-        self.make_move(mv, result);
+    fn has_insufficient_material(&self) -> bool {
+        self.pieces(Piece::Pawn).0 == 0
+            && self.pieces(Piece::Queen).0 == 0
+            && self.pieces(Piece::Rook).0 == 0
+            && self.pieces(Piece::Bishop).popcnt() + self.pieces(Piece::Knight).popcnt() <= 1
     }
 }
