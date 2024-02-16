e9fcf01f31e10c7f19d3cf200012f2e09c08815a
Bendik Samseth <b.samseth@gmail.com>
2024-02-17 00:11:43 +0100

    Cleaner impl for MVV/LVA
    No functional change, only slightly less ugly.

--- goldfish/engine/src/movelist.rs
+++ goldfish/engine/src/movelist.rs
@@ -19,34 +19,38 @@ impl MoveEntry {
             value: -Value::INFINITE,
         }
     }
+}
 
-    pub fn new_with_rating(mv: ChessMove, board: &chess::Board) -> Self {
-        let mut entry = Self::new(mv);
-        entry.rate_move(board);
-        entry
+impl MoveVec {
+    pub fn mvv_lva_rated(mut self, board: &Board) -> Self {
+        for entry in self.iter_mut() {
+            entry.value = Value::new(0);
+
+            if let Some(target) = board.piece_on(entry.mv.get_dest()) {
+                entry.value = Value::new(
+                    10 * piece_value(target)
+                        - piece_value(
+                            board
+                                .piece_on(entry.mv.get_source())
+                                .expect("there should always be a source piece for a move"),
+                        ),
+                );
+            }
+        }
+        self
     }
 
-    /// Rate move base on MVV/LVA.
-    ///
-    /// Most valuable victim / least valuable attacker (MVV/LVA) is a heuristic for ordering captures.
-    /// The idea is to capture the most valuable victim with the least valuable attacker.
-    /// Not a perfect heuristic, but much better than random/generation order.
-    fn rate_move(&mut self, board: &chess::Board) {
-        self.value = Value::new(0);
+    pub fn sort(&mut self) {
+        self.0.sort_by_key(|m| -m.value);
+    }
 
-        if let Some(target) = board.piece_on(self.mv.get_dest()) {
-            self.value = Value::new(
-                10 * piece_value(target)
-                    - piece_value(
-                        board
-                            .piece_on(self.mv.get_source())
-                            .expect("there should always be a source piece for a move"),
-                    ),
-            );
-        }
+    pub fn sorted(mut self) -> Self {
+        self.sort();
+        self
     }
 }
 
+/// Piece values for use with MVV/LVA only.
 const fn piece_value(piece: Piece) -> i16 {
     match piece {
         Piece::Pawn => 100,
@@ -58,25 +62,9 @@ const fn piece_value(piece: Piece) -> i16 {
     }
 }
 
-impl MoveVec {
-    pub fn new_from_moves(move_gen: MoveGen, board: &Board) -> Self {
-        Self(
-            move_gen
-                .map(|mv| MoveEntry::new_with_rating(mv, board))
-                .collect(),
-        )
-    }
-
-    pub fn new_from_board(board: &Board) -> Self {
-        Self::new_from_moves(MoveGen::new_legal(board), board)
-    }
-
-    pub fn rate_moves(&mut self, board: &chess::Board) {
-        self.iter_mut().for_each(|entry| entry.rate_move(board));
-    }
-
-    pub fn sort_moves(&mut self) {
-        self.0.sort_by_key(|m| -m.value);
+impl From<MoveGen> for MoveVec {
+    fn from(move_gen: MoveGen) -> Self {
+        Self(move_gen.map(MoveEntry::new).collect())
     }
 }
 
