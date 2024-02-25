858508cac549cfc64c8b47fda03b4a73b1382aac
Bendik Samseth <b.samseth@gmail.com>
2024-02-25 22:50:09 +0100

    Fix root move tablebase filtering
    Needed to account for the WDL returned by fathom being side-to-move
    oriented. This caused filtering to only keep moves that didn't keep the
    original WDL.

--- goldfish/engine/src/tb/root_probe.rs
+++ goldfish/engine/src/tb/root_probe.rs
@@ -2,6 +2,7 @@ use super::{Tablebase, Wdl};
 use crate::board::BoardExt;
 use crate::movelist::MoveVec;
 
+#[derive(Debug)]
 pub struct RootMovesFilter<'a> {
     wdl: Wdl,
     tb: &'a Tablebase,
@@ -26,6 +27,13 @@ impl<'a> RootMovesFilter<'a> {
 
     /// Filter the provided `moves` to only include moves that preserve the [`Wdl`] value.
     pub fn filter_moves(&self, moves: &mut MoveVec) {
+        let wdl_opponent = match self.wdl {
+            Wdl::Win => Wdl::Loss,
+            Wdl::Loss => Wdl::Win,
+            Wdl::Draw => Wdl::Draw,
+            Wdl::BlessedLoss => Wdl::CursedWin,
+            Wdl::CursedWin => Wdl::BlessedLoss,
+        };
         moves.retain(|mv| {
             let halfmove_clock = if self.board.halfmove_reset(mv.mv) {
                 0
@@ -35,7 +43,7 @@ impl<'a> RootMovesFilter<'a> {
             let next = self.board.make_move_new(mv.mv);
             self.tb
                 .probe_wdl(&next, halfmove_clock)
-                .map_or(false, |wdl| wdl == self.wdl)
+                .map_or(false, |wdl| wdl == wdl_opponent)
         });
     }
 }
