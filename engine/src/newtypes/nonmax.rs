75d67046afac5697db7aa9f615818969a4efc093
Bendik Samseth <b.samseth@gmail.com>
2024-07-02 22:44:14 +0200

    Use NonMax16 for Value

--- engine/src/newtypes/nonmax.rs
+++ engine/src/newtypes/nonmax.rs
@@ -3,7 +3,7 @@
 //! This is the equivalent of `NonZeroI16` but with `i16::MAX` as the invalid value.
 //! Only the [`i16`] variant is implemented, as this is the only one needed for the engine.
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct NonMaxI16(std::num::NonZeroI16);
 
 impl NonMaxI16 {
@@ -16,10 +16,17 @@ impl NonMaxI16 {
         }
     }
 
-    pub const fn new_unchecked(n: i16) -> Self {
+    pub const unsafe fn new_unchecked(n: i16) -> Self {
         // SAFETY: Caller guarantees that `n` is not `i16::MAX`.
         Self(unsafe { std::num::NonZeroI16::new_unchecked(n ^ i16::MAX) })
     }
+
+    pub const fn get(self) -> i16 {
+        self.0.get() ^ i16::MAX
+    }
+
+    pub const MAX: Self = unsafe { Self::new_unchecked(i16::MAX - 1) };
+    pub const MIN: Self = unsafe { Self::new_unchecked(i16::MIN) };
 }
 
 impl From<NonMaxI16> for i16 {
@@ -28,9 +35,23 @@ impl From<NonMaxI16> for i16 {
     }
 }
 
+impl From<u8> for NonMaxI16 {
+    fn from(n: u8) -> Self {
+        // SAFETY: `n` cannot be `i16::MAX`.
+        unsafe { Self::new_unchecked(n as i16) }
+    }
+}
+
 impl std::fmt::Display for NonMaxI16 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         i16::from(*self).fmt(f)
+    }
+}
+
+impl Default for NonMaxI16 {
+    fn default() -> Self {
+        // SAFETY: `0` is not `i16::MAX`.
+        unsafe { Self::new_unchecked(0) }
     }
 }
 
@@ -45,7 +66,7 @@ impl std::ops::Add for NonMaxI16 {
     fn add(self, rhs: Self) -> Self {
         let sum = i16::from(self) + i16::from(rhs);
         debug_assert_ne!(sum, i16::MAX);
-        Self::new_unchecked(sum)
+        unsafe { Self::new_unchecked(sum) }
     }
 }
 
@@ -55,7 +76,7 @@ impl std::ops::Sub for NonMaxI16 {
     fn sub(self, rhs: Self) -> Self {
         let diff = i16::from(self) - i16::from(rhs);
         debug_assert_ne!(diff, i16::MAX);
-        Self::new_unchecked(diff)
+        unsafe { Self::new_unchecked(diff) }
     }
 }
 
@@ -65,7 +86,7 @@ impl std::ops::Neg for NonMaxI16 {
     fn neg(self) -> Self {
         let neg = -i16::from(self);
         debug_assert_ne!(neg, i16::MAX);
-        Self::new_unchecked(neg)
+        unsafe { Self::new_unchecked(neg) }
     }
 }
 
@@ -94,97 +115,70 @@ impl std::str::FromStr for NonMaxI16 {
 mod tests {
     use super::*;
 
+    fn nu(n: i16) -> NonMaxI16 {
+        debug_assert_ne!(n, i16::MAX);
+        // SAFETY: `n` is not `i16::MAX`.
+        unsafe { NonMaxI16::new_unchecked(n) }
+    }
+
     #[test]
     fn test_nonmaxi16_new() {
         assert_eq!(NonMaxI16::new(i16::MAX), None);
-        assert_eq!(
-            NonMaxI16::new(i16::MIN),
-            Some(NonMaxI16::new_unchecked(i16::MIN))
-        );
-        assert_eq!(NonMaxI16::new(0), Some(NonMaxI16::new_unchecked(0)));
-        assert_eq!(NonMaxI16::new(1), Some(NonMaxI16::new_unchecked(1)));
-        assert_eq!(
-            NonMaxI16::new(i16::MAX - 1),
-            Some(NonMaxI16::new_unchecked(i16::MAX - 1))
-        );
+        assert_eq!(NonMaxI16::new(i16::MIN), Some(nu(i16::MIN)));
+        assert_eq!(NonMaxI16::new(0), Some(nu(0)));
+        assert_eq!(NonMaxI16::new(1), Some(nu(1)));
+        assert_eq!(NonMaxI16::new(i16::MAX - 1), Some(nu(i16::MAX - 1)));
     }
 
     #[test]
     fn test_nonmaxi16_from() {
-        assert_eq!(i16::from(NonMaxI16::new_unchecked(i16::MIN)), i16::MIN);
-        assert_eq!(i16::from(NonMaxI16::new_unchecked(0)), 0);
-        assert_eq!(i16::from(NonMaxI16::new_unchecked(1)), 1);
-        assert_eq!(
-            i16::from(NonMaxI16::new_unchecked(i16::MAX - 1)),
-            i16::MAX - 1
-        );
+        assert_eq!(i16::from(nu(i16::MIN)), i16::MIN);
+        assert_eq!(i16::from(nu(0)), 0);
+        assert_eq!(i16::from(nu(1)), 1);
+        assert_eq!(i16::from(nu(i16::MAX - 1)), i16::MAX - 1);
     }
 
     #[test]
     fn test_nonmaxi16_add() {
-        assert_eq!(
-            NonMaxI16::new_unchecked(1) + NonMaxI16::new_unchecked(2),
-            NonMaxI16::new_unchecked(3)
-        );
-        assert_eq!(
-            NonMaxI16::new_unchecked(i16::MAX - 2) + NonMaxI16::new_unchecked(1),
-            NonMaxI16::new_unchecked(i16::MAX - 1)
-        );
+        assert_eq!(nu(1) + nu(2), nu(3));
+        assert_eq!(nu(i16::MAX - 2) + nu(1), nu(i16::MAX - 1));
     }
 
     #[test]
     fn test_nonmaxi16_sub() {
-        assert_eq!(
-            NonMaxI16::new_unchecked(3) - NonMaxI16::new_unchecked(2),
-            NonMaxI16::new_unchecked(1)
-        );
-        assert_eq!(
-            NonMaxI16::new_unchecked(i16::MAX - 1) - NonMaxI16::new_unchecked(2),
-            NonMaxI16::new_unchecked(i16::MAX - 3)
-        );
+        assert_eq!(nu(3) - nu(2), nu(1));
+        assert_eq!(nu(i16::MAX - 1) - nu(2), nu(i16::MAX - 3));
     }
 
     #[test]
     fn test_nonmaxi16_neg() {
         for case in [1, 0, i16::MAX - 1, i16::MIN + 2].iter().copied() {
-            assert_eq!(
-                -NonMaxI16::new_unchecked(case),
-                NonMaxI16::new_unchecked(-case)
-            );
+            assert_eq!(-nu(case), nu(-case));
         }
     }
 
     #[test]
     fn test_nonmaxi16_add_assign() {
-        let mut n = NonMaxI16::new_unchecked(1);
-        n += NonMaxI16::new_unchecked(2);
-        assert_eq!(n, NonMaxI16::new_unchecked(3));
+        let mut n = nu(1);
+        n += nu(2);
+        assert_eq!(n, nu(3));
     }
 
     #[test]
     fn test_nonmaxi16_sub_assign() {
-        let mut n = NonMaxI16::new_unchecked(3);
-        n -= NonMaxI16::new_unchecked(2);
-        assert_eq!(n, NonMaxI16::new_unchecked(1));
+        let mut n = nu(3);
+        n -= nu(2);
+        assert_eq!(n, nu(1));
     }
 
     #[test]
     fn test_nonmaxi16_from_str() {
-        assert_eq!(
-            "1".parse::<NonMaxI16>().unwrap(),
-            NonMaxI16::new_unchecked(1)
-        );
-        assert_eq!(
-            "0".parse::<NonMaxI16>().unwrap(),
-            NonMaxI16::new_unchecked(0)
-        );
-        assert_eq!(
-            "-1".parse::<NonMaxI16>().unwrap(),
-            NonMaxI16::new_unchecked(-1)
-        );
+        assert_eq!("1".parse::<NonMaxI16>().unwrap(), nu(1));
+        assert_eq!("0".parse::<NonMaxI16>().unwrap(), nu(0));
+        assert_eq!("-1".parse::<NonMaxI16>().unwrap(), nu(-1));
         assert_eq!(
             format!("{}", i16::MAX - 1).parse::<NonMaxI16>().unwrap(),
-            NonMaxI16::new_unchecked(i16::MAX - 1)
+            nu(i16::MAX - 1)
         );
         assert!(format!("{}", i16::MAX).parse::<NonMaxI16>().is_err());
     }
