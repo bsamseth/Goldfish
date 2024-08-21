8565600dddcd167cafd667a04f9e8ca8ab7272a2
Bendik Samseth <b.samseth@gmail.com>
2024-08-21 16:11:55 +0200

    Add match history for rc8
    Head to head statistics:
    
     1) Goldfish v2.0.0-rc8 2104.2 :   1362 (+625,=479,-258),  63.5 %
    
        vs.                        :  games (   +,   =,   -),   (%) :    Diff
        Goldfish v2.0.0-rc7        :    682 ( 277, 296, 109),  62.3 :   +73.8
        Goldfish v1.13.0           :    680 ( 348, 183, 149),  64.6 :  +121.2
    
    ...
    
       # PLAYER                 : RATING    POINTS  PLAYED    (%)
       1 Goldfish v2.0.0-rc8    : 2104.2     864.5    1362   63.5%
       2 Goldfish v2.0.0-rc7    : 2030.4     687.0    1332   51.6%
       3 Goldfish v1.13.0       : 1983.0    1348.0    2622   51.4%
       4 Goldfish v1.12.1       : 1932.0     748.0    1613   46.4%
       5 Goldfish v1.12.0       : 1906.3     833.5    1528   54.5%
       6 Goldfish v1.11.1       : 1893.0    1095.0    2015   54.3%
       7 Goldfish v1.9.0        : 1878.3    1826.5    3538   51.6%
       8 Goldfish v1.11.0       : 1878.0     549.5    1100   50.0%
       9 Goldfish v2.0.0-rc6    : 1861.2     136.0     338   40.2%
      10 Goldfish v2.0.0-rc3    : 1853.6     177.5     417   42.6%
      11 Goldfish v2.0.0-rc5    : 1848.7     120.5     316   38.1%
      12 Goldfish v2.0.0-rc1    : 1835.2     217.5     436   49.9%
      13 Goldfish v2.0.0-rc2    : 1833.9     141.0     354   39.8%
      14 Goldfish v1.7.0        : 1813.7    1086.5    2053   52.9%
      15 Goldfish v1.8.2        : 1808.8     392.0     783   50.1%
      16 Goldfish v1.7.1        : 1806.6     244.0     477   51.2%
      17 Goldfish v1.8.0        : 1799.2     324.0     650   49.8%
      18 Goldfish v2.0.0-rc4    : 1798.8      19.5      57   34.2%
      19 Goldfish v1.8.1        : 1796.2     485.5    1000   48.5%
      20 Goldfish v1.6.0        : 1783.7     625.0    1151   54.3%
      21 Goldfish v1.7.2        : 1780.8      69.5     150   46.3%
      22 Goldfish v1.5.1        : 1726.0     645.5    1435   45.0%
      23 Goldfish v1.5          : 1723.7     554.5    1145   48.4%
      24 Goldfish v1.4          : 1720.9     646.5    1325   48.8%
      25 Goldfish v1.3          : 1706.2     314.0     680   46.2%
      26 Goldfish v1.2          : 1675.7     237.5     585   40.6%
      27 Goldfish v1.1          : 1634.5     210.0     597   35.2%
      28 Goldfish v1.0          : 1629.2     129.5     397   32.6%
    
    White advantage = 15.60
    Draw rate (equal opponents) = 45.54 %

--- stats/duel.sh
+++ stats/duel.sh
@@ -2,6 +2,8 @@
 
 cutechess-cli \
 	-engine \
+	conf='Goldfish v2.0.0-rc8' \
+	-engine \
 	conf='Goldfish v2.0.0-rc7' \
 	-engine \
 	conf='Goldfish v1.13.0' \
@@ -12,8 +14,9 @@ cutechess-cli \
 	option.SyzygyPath=/home/bendik/goldfish/syzygy \
 	book=/home/bendik/goldfish/openingbooks/gm2001.bin \
 	bookdepth=6 \
-	-games 4 \
-	-rounds 80 \
+	-games 2 \
+	-rounds 500 \
 	-pgnout /home/bendik/goldfish/stats/match-history.pgn min "fi" \
 	-concurrency 10 \
-	-tournament gauntlet
+	-ratinginterval 5 \
+	-tournament gauntlet \
