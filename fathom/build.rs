364a4395de9ecac810bfa636641e8adb43a52a42
Bendik Samseth <b.samseth@gmail.com>
2024-09-16 09:35:54 +0200

    Fix fathom build on Windows
    Apparently MSVC needs to be told to use C++ mode for the C files,
    as they are somehow more modern than what MSCV can handle.

--- fathom/build.rs
+++ fathom/build.rs
@@ -3,6 +3,7 @@ fn main() {
         .include("external/fathom/src")
         .file("external/fathom/src/tbprobe.c")
         .flag("-DTB_NO_HELPER_API")
+        .flag_if_supported("-TP")
         .compile("fathom");
     println!("cargo:rerun-if-changed=external/fathom");
 }
